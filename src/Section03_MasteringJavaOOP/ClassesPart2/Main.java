068f4e34cd908207650c4b83a9b878a50bfb3304
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2025-12-21 18:47:42 +0100

    03.02.Classes Part 2

---
+++ src/Section03_MasteringJavaOOP/ClassesPart2/Main.java
@@ -0,0 +1,16 @@
+package Section03_MasteringJavaOOP.ClassesPart2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+//        car.make = "Porsche";
+//        car.model = "Carrera";
+//        car.color = "Red";
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+    }
+}
+
