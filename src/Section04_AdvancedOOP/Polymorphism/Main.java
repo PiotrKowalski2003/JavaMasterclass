a85caea42875b003e8b439002c03c640733c6bb7
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2025-12-24 21:18:16 +0100

    04.07.Polymorphism

---
+++ src/Section04_AdvancedOOP/Polymorphism/Main.java
@@ -0,0 +1,11 @@
+package Section04_AdvancedOOP.Polymorphism;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+    }
+}
+
