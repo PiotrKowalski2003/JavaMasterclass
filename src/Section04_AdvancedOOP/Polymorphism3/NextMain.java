2e2c9bfd8ca3aa56ab3d0c5cf82f68a28e2dc183
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2025-12-25 12:02:05 +0100

    04.09.Polymorphism 3

---
+++ src/Section04_AdvancedOOP/Polymorphism3/NextMain.java
@@ -0,0 +1,24 @@
+package Section04_AdvancedOOP.Polymorphism3;
+
+public class NextMain {
+
+    public static void main(String[] args) {
+
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "Airplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C", "Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+    }
+}
+
