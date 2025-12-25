5a8fc0a77244f9c68ad836a3e89cba97d433d224
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2025-12-25 12:35:23 +0100

    04.16.Packages

---
+++ src/Section04_AdvancedOOP/Packages/com/abc/first/Item.java
@@ -0,0 +1,18 @@
+package Section04_AdvancedOOP.Packages.com.abc.first;
+
+public class Item {
+
+    private String type;
+
+    public Item(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
+
---
+++ src/Section04_AdvancedOOP/Packages/dev/Ipa/Main.java
@@ -0,0 +1,13 @@
+package Section04_AdvancedOOP.Packages.dev.Ipa;
+
+import Section04_AdvancedOOP.Packages.com.abc.first.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
+
