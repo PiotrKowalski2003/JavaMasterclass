8497c177c860f8fe1ea607e38d71eba6fad304f9
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2025-12-30 10:53:01 +0100

    07.01.Abstract Classes

---
+++ src/Section07_AbstractionAndInterfaces/AbstractClasses/Dog.java
@@ -0,0 +1,19 @@
+package Section07_AbstractionAndInterfaces.AbstractClasses;
+
+public class Dog extends Animal {
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
+
---
+++ src/Section07_AbstractionAndInterfaces/AbstractClasses/Main.java
@@ -0,0 +1,8 @@
+package Section07_AbstractionAndInterfaces.AbstractClasses;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
