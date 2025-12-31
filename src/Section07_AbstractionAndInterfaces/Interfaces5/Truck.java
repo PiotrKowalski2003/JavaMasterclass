7244ab042684600e5193300f8eae50e3db127ff6
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2025-12-31 11:37:59 +0100

    07.10.Interfaces 5

---
+++ src/Section07_AbstractionAndInterfaces/Interfaces5/Truck.java
@@ -0,0 +1,9 @@
+package Section07_AbstractionAndInterfaces.Interfaces5;
+
+public class Truck implements Trackable {
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
+    }
+}
