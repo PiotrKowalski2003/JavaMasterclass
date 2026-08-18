fc073cf7020df5f76ec2562ec161c54e1c649703
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-08-18 18:13:16 +0200

    11.02.Collections Methods

---
+++ src/Section11_FrameworkListsSetsMaps/CollectionsMethods/Main.java
@@ -0,0 +1,14 @@
+package Section11_FrameworkListsSetsMaps.CollectionsMethods;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+    }
+}
+
